70af5e6a1233f343308d73bdb2ff8b199099b46e
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-04-07 09:50:03 -0300

    hicimos tres rutas con argumentos

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ def hello_world():
 @app.route("/chau")
 def chau():
     return "<p>Chau</p>"
+
+@app.route("/hola/<string:nombre>")#string necesita un string
+def nombre(nombre):
+    return f"<p>hola {nombre}</p>"
+
+@app.route("/tirar-dado/<int:caras>") #int si o si necesita un numero
+def dado(caras):
+    from random import randint
+    h = randint(1, caras)
+    return f"<p>tire un  dado de {caras}, salio {h}</p>"
+
+#siempre que haya un string,float,int, en la ruta, quiere decir que necsesita recibir algo y se separa po0r barras '/'
