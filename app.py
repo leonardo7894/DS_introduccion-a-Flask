03b860fa00c13def999894ce2512631647516db3
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-04-07 11:58:03 -0300

    creamos una pagina con links utilizado la herramienta 'url_for'

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask ,url_for
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def main():
+    url_hola = url_for("hola")
+    url_nombre = url_for("nombre", nombre = "leonardo")
+    url_dados = url_for("dado", caras=6)
+    url_imagen = url_for("static", filename="logo.jpg")
+    url_chau = url_for("chau")
+    return f"""
+    <a href ="{url_hola}">hola</a>
+    <br>
+    <a href ="{url_nombre}">nombre</a>
+    <br>
+    <a href ="{url_dados}">dados</a>
+    <br>
+    <a href ="{url_imagen}">imagen</a>
+    <br>
+    <a href ="{url_chau}">chau</a>
+"""
+
 @app.route("/hola")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hola():
+    return "<p>hola leonardo</p>"
 
 @app.route("/chau")
 def chau():
@@ -20,9 +40,12 @@ def dado(caras):
     h = randint(1, caras)
     return f"<p>tire un  dado de {caras}, salio {h}</p>"
 
-@app.route("/suma/<int:n1, n2>")#string necesita un string
+@app.route("/suma/<int:n1>/<int:n2>")#string necesita un string
 def suma(n1, n2):
     s= n1+n2
-    return f"<p>{n1} + {n2} es {s}</p>"
+    return f"<p>{n1} + {n2} = {s}</p>"
 
 #siempre que haya un string,float,int, en la ruta, quiere decir que necsesita recibir algo y se separa po0r barras '/'
+
+
+
